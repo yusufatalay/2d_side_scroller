02e3d6cb897bc234a15ed1ca43d9e6cd8d9e2959
Yusuf <yusuftahaatalay14@gmail.com>
2020-08-24 16:36:56 +0300

    finished wlk and jump behaviours

---
+++ Assets/scripts/JumpBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class JumpBehaviour : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D playerRigidBody;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private bool isSpacePressed; 
+    
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isSpacePressed = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isSpacePressed = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isSpacePressed && isGrounded)
+        {
+            playerRigidBody.AddForce(Vector2.up*jumpForce*Time.deltaTime,ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+       
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
+        
+    }
+}
---
+++ Assets/scripts/WalkBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkBehaviour : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    [SerializeField] private float horizontalInput;
+     
+ 
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(horizontalInput*speed*Time.deltaTime,0));
+    }
+}
