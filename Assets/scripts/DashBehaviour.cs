797ed989c88a8413606f61d6c8a17107c8f86df8
Yusuf <yusuftahaatalay14@gmail.com>
2020-08-30 16:19:33 +0300

    tried to improvise the dashing move but failed ):

--- Assets/scripts/DashBehaviour.cs
+++ Assets/scripts/DashBehaviour.cs
@@ -1,102 +1,43 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DashBehaviour : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D playerRigidBody;
-
-    [Header("Dash Settings")] [SerializeField] private float dashingSpeed;
-
-    [SerializeField] private float dashTime;
-    [SerializeField] private float startDashTime;
-    [SerializeField] private int direction;
+    public float dashCooldown;
+    public bool canIDash = true;
+    public float dashSpeed;
+    public Vector2 savedVelocity;
+    public Rigidbody2D playerRigidBody;
 
     private void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
-        dashTime = startDashTime;
     }
 
-    private void Update()
+    void Update()
     {
-        if (direction == 0)
+        Debug.Log(playerRigidBody.velocity);
+        if (dashCooldown > 0)
         {
-            if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                direction = 5;
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                direction = 6;
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                direction = 7;
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                direction = 8;
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                direction = 1;
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                direction = 2;
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                direction = 3;
-            }
-            else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                direction = 4;
-            }
+            canIDash = false;
+            dashCooldown -= Time.deltaTime;
+            //if I put saved velocity here, it will continue to move at savedVelocity until dashCooldown = 0? so it can't go here? right?
         }
-        else
-        {
-            if (dashTime <= 0)
-            {
-                direction = 0;
-                dashTime = startDashTime;
-                playerRigidBody.velocity = Vector2.zero;
-            }
-            else
-            {
-                dashTime -= Time.deltaTime;
 
-                switch (direction)
-                {
-                    case 1:
-                        playerRigidBody.velocity = Vector2.left * dashingSpeed;
-                        break;
-                    case 2:
-                        playerRigidBody.velocity = Vector2.right * dashingSpeed;
-                        break;
-                    case 3:
-                        playerRigidBody.velocity = Vector2.up * dashingSpeed;
-                        break;
-                    case 4:
-                        playerRigidBody.velocity = Vector2.down * dashingSpeed;
-                        break;
-                    case 5:
-                        playerRigidBody.velocity = new Vector2(1,1) * dashingSpeed;
-                        break;
-                    case 6:
-                        playerRigidBody.velocity = new Vector2(-1,1) * dashingSpeed;
-                        break;
-                    case 7:
-                        playerRigidBody.velocity = new Vector2(1,-1) * dashingSpeed;
-                        break;
-                    case 8:
-                        playerRigidBody.velocity = new Vector2(-1,-1) * dashingSpeed;
-                        break;
-                }
-                
-            }
+        if (dashCooldown <= 0)
+        {
+            canIDash = true;
+            //if I put savedVelocity here it doesn't return to savedVelocity until dashCooldown <=0 so... it doesn't go here either right...?
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canIDash == true)
+        {
+            //saves velocity prior to dashing
+           
+            //this part is the actual dash itself
+            playerRigidBody.AddForce(new Vector2( dashSpeed, 0),ForceMode2D.Impulse);
+            //sets up a cooldown so you have to wait to dash again
+            dashCooldown = 2;
         }
     }
 }
