f7b0efa6a52f33574e733ab290d1918c08e19fc4
Yusuf <yusuftahaatalay14@gmail.com>
2020-08-31 15:51:11 +0300

    disabled the dashBehaviour because it ain't working ;(

--- Assets/scripts/DashBehaviour.cs
+++ Assets/scripts/DashBehaviour.cs
@@ -3,20 +3,27 @@ using UnityEngine;
 
 public class DashBehaviour : MonoBehaviour
 {
+    [Header("Dash Settings")] [SerializeField]
+    private float dashingSpeed;
+
+    [SerializeField] private float dashTime;
+    [SerializeField] private float startDashTime;
+    [SerializeField] private int direction;
+
     public float dashCooldown;
     public bool canIDash = true;
-    public float dashSpeed;
-    public Vector2 savedVelocity;
+
+
     public Rigidbody2D playerRigidBody;
 
     private void Start()
     {
         playerRigidBody = GetComponent<Rigidbody2D>();
+        dashTime = startDashTime;
     }
 
     void Update()
     {
-        Debug.Log(playerRigidBody.velocity);
         if (dashCooldown > 0)
         {
             canIDash = false;
@@ -30,14 +37,99 @@ public class DashBehaviour : MonoBehaviour
             //if I put savedVelocity here it doesn't return to savedVelocity until dashCooldown <=0 so... it doesn't go here either right...?
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && canIDash == true)
+        if (canIDash == true)
         {
-            //saves velocity prior to dashing
            
-            //this part is the actual dash itself
-            playerRigidBody.AddForce(new Vector2( dashSpeed, 0),ForceMode2D.Impulse);
-            //sets up a cooldown so you have to wait to dash again
-            dashCooldown = 2;
+            if (direction == 0)
+            {
+                if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightArrow) &&
+                    Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    direction = 5;
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.LeftArrow) &&
+                         Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    direction = 6;
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightArrow) &&
+                         Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    direction = 7;
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.LeftArrow) &&
+                         Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    direction = 8;
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    direction = 1;
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    direction = 2;
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    direction = 3;
+                }
+                else if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    direction = 4;
+                }
+            }
+            else
+            {
+                if (dashTime <= 0)
+                {
+                    direction = 0;
+                    dashTime = startDashTime;
+                    playerRigidBody.velocity = Vector2.zero;
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+
+                    switch (direction)
+                    {
+                        case 1:
+                            playerRigidBody.velocity = Vector2.left * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                        case 2:
+                            playerRigidBody.velocity = Vector2.right * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                        case 3:
+                            playerRigidBody.velocity = Vector2.up * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                        case 4:
+                            playerRigidBody.velocity = Vector2.down * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                        case 5:
+                            playerRigidBody.velocity = new Vector2(1, 1) * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                        case 6:
+                            playerRigidBody.velocity = new Vector2(-1, 1) * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                        case 7:
+                            playerRigidBody.velocity = new Vector2(1, -1) * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                        case 8:
+                            playerRigidBody.velocity = new Vector2(-1, -1) * dashingSpeed;
+                            dashCooldown = 2;
+                            break;
+                    }
+                }
+            }
+
+            
         }
     }
 }
